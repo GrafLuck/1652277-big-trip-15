2377d5019822217b7f19f09511700b4f4e2a0d72
Dmitriy Lutsishin <graf.luck@gmail.com>
2021-07-27 23:36:44 +0300

    Добавил модули Фильтров, сортировки и информации о поездке

--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
 import { createSiteMenuTemplate } from './view/site-menu.js';
+import { createTripInfoTemplate } from './view/trip-info.js';
+import { createFiltersTemplate } from './view/filters.js';
+import { createSortingTemplate } from './view/sorting.js';
 
 const siteNavigationElement = document.querySelector('.trip-controls__navigation');
+const tripMainElement = document.querySelector('.trip-main');
+const filtersElement = document.querySelector('.trip-controls__filters');
+const tripEventsElement = document.querySelector('.trip-events');
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
 
 render(siteNavigationElement, createSiteMenuTemplate(), 'beforeend');
+render(tripMainElement, createTripInfoTemplate(), 'afterbegin')
+render(filtersElement, createFiltersTemplate(), 'beforeend');
+render(tripEventsElement, createSortingTemplate(), 'beforeend');
