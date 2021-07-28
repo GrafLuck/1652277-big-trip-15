ff570b9dc1537f904121d86593bc90449f07500b
Dmitriy Lutsishin <graf.luck@gmail.com>
2021-07-28 22:28:42 +0300

    Добавил модули создания и редактирования события и модуль точки маршрута

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import { createSiteMenuTemplate } from './view/site-menu.js';
 import { createTripInfoTemplate } from './view/trip-info.js';
 import { createFiltersTemplate } from './view/filters.js';
 import { createSortingTemplate } from './view/sorting.js';
+import { createEventListTemplate } from './view/event-list.js';
+import { createAddEventTemplate } from './view/new-event.js';
+import { createEditEventTemplate } from './view/editable-event.js';
+import { createRoutePointTemplate } from './view/route-point.js';
 
 const siteNavigationElement = document.querySelector('.trip-controls__navigation');
 const tripMainElement = document.querySelector('.trip-main');
@@ -16,3 +20,12 @@ render(siteNavigationElement, createSiteMenuTemplate(), 'beforeend');
 render(tripMainElement, createTripInfoTemplate(), 'afterbegin')
 render(filtersElement, createFiltersTemplate(), 'beforeend');
 render(tripEventsElement, createSortingTemplate(), 'beforeend');
+render(tripEventsElement, createEventListTemplate(), 'beforeend');
+
+const tripEventsListElement = document.querySelector('.trip-events__list');
+
+render(tripEventsListElement, createEditEventTemplate(), 'beforeend');
+render(tripEventsListElement, createAddEventTemplate(), 'beforeend');
+for (let i = 0; i < 3; i++) {
+  render(tripEventsListElement, createRoutePointTemplate(), 'beforeend');
+}
