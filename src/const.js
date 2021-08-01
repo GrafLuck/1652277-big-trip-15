ea5654f41e5baf64f8f0ebe304a18193c17e1b0d
Dmitriy Lutsishin <graf.luck@gmail.com>
2021-08-01 19:59:19 +0300

    Выделил константы времени в отдельный модуль

---
+++ src/const.js
@@ -0,0 +1,15 @@
+const Time = {
+  HOURS_PER_DAY: 24,
+  MINUTES_PER_HOUR: 60,
+  MINUTES_PER_DAY: 1440,
+};
+
+const DateFormat = {
+  ONLY_DATE_MAIN: 'YYYY-MM-DD',
+  ONLY_DATE_SECONDARY: 'MMM DD',
+  FULL_DATE: 'DD/MM/YY HH:mm',
+  DATETIME_ATTRIBUTE: 'YYYY-MM-DD[T]HH:mm',
+  ONLY_TIME: 'HH:mm',
+};
+
+export { Time, DateFormat };
